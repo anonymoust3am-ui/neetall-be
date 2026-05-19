38404043ea1537f479dc24abc72a3ed8a9c6dbe3
anonymoust3am-ui <anonymoust3am@gmail.com>
2026-05-19 13:57:24 +0530

    feat: add AppService with initial health check endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Neetall Backend server is running!';
+    return 'Neetall Backend server is running successfully !';
   }
 }
