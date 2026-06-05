02e659ff76359241b91fe995ad9c34e27d0afca2
anonymoust3am-ui <anonymoust3am@gmail.com>
2026-06-05 19:16:56 +0530

    corse

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'data'), {
     prefix: '/data/',
   });
-  app.enableCors({
-    origin: [process.env.FRONTEND_URL ? process.env.FRONTEND_URL : "http://localhost:3000", "http://192.168.29.243:3000", "http://127.0.0.1:3000", "https://neetell.in", "http://72.61.233.53:3000", "http://72.61.233.53:80", "http://72.61.233.53:8080"],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
